2b8719b91ae0da94bd805331362e48145421317b
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-01-31 21:10:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const signupButton=document.getElementById(`signupbutton`);
+const signinButton=document.getElementById(`siginbutton`);
+const signinform=document.getElementById(`signin`);
+const signupform=document.getElementById(`signup`)
+
+
